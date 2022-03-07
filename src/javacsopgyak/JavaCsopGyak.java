700ab2cd1e75a08b9ca6b107b4f24fe52a6a9bac
janos0348 <78791817+janos0348@users.noreply.github.com>
2022-03-07 12:36:24 +0100

    Nevek deklarálása

--- src/javacsopgyak/JavaCsopGyak.java
+++ src/javacsopgyak/JavaCsopGyak.java
@@ -15,7 +15,8 @@ public class JavaCsopGyak {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String nev1;
+        String nev2;
     }
     
 }
